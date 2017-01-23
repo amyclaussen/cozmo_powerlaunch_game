0401997d1fa27448c4f22d9e69810f19f08a0b60
amyclaussen <aeclaussen@gmail.com>
2017-01-22 17:34:08 -0800

    cozmo does knock over cubes

--- powerlaunch.py
+++ powerlaunch.py
@@ -1,7 +1,9 @@
 """
 POWERLAUNCH
 
-With this minigame, power up Cozmo and launch your little buddy towards a target! Don't worry, you can't hurt him - powerboosing is one of Cozmo's favorites.
+Ready...Aim...POWER!
+
+With this minigame, power up Cozmo and launch your little buddy towards a cube tower target! Don't worry, you can't hurt him - powerlaunching is one of Cozmo's favorites.
 
 This is a variation on an archery or golf minigame.
 """
@@ -28,7 +30,9 @@ class PowerlaunchGame(object):
     	self.list_of_identified_cubes = []
     	self.successfully_found_cubes_check = None
     	self.finished_stacking_cubes = None
+    	self.random_distance_from_target = None
     	self.user_defined_launch_power = None
+    	self.did_win = False
 
     def identify_cubes_and_create_list(self, robot: cozmo.robot.Robot):
 
@@ -77,19 +81,15 @@ class PowerlaunchGame(object):
         color_cycle.run_color_cycle(robot, cycle_time_in_seconds, cube)
 
 
-    def move_into_launch_position(self, robot: cozmo.robot.Robot):
+    def move_into_launch_position(self, robot: cozmo.robot.Robot, distance_range_tuple, angle_range_tuple):
 
     	#creates random distance away from target, within distance range
-    	random_distance_from_target = random.randint(distance_range_tuple[0], distance_range_tuple[1])
-    	print("moving", random_distance_from_target, "mm away from target")
+    	self.random_distance_from_target = random.randint(distance_range_tuple[0], distance_range_tuple[1])
+    	print("moving", self.random_distance_from_target, "mm away from target")
 
     	#moves cozmo a random distance away from target, within distance range
-    	drive_cozmo_straight(robot, -(random_distance_from_target), 50)
+    	drive_cozmo_straight(robot, -(self.random_distance_from_target), 50)
 
-	# def update_lift(self, robot: cozmo.robot.Robot):
-	# 	lift_speed = self.pick_speed(8, 4, 2)
-	# 	lift_vel = (self.lift_up - self.lift_down) * lift_speed
-	# 	self.cozmo.move_lift(lift_vel)
 
     def launch_cozmo_towards_target(self, robot: cozmo.robot.Robot, distance_range_tuple, angle_range_tuple):
 
@@ -107,6 +107,9 @@ class PowerlaunchGame(object):
 
     	drive_cozmo_straight(robot, launch_distance, launch_speed)
 
+    	if launch_distance > self.random_distance_from_target + 30:
+    		self.did_win = True
+
 
 def cozmo_program(robot: cozmo.robot.Robot):
 
@@ -115,28 +118,31 @@ def cozmo_program(robot: cozmo.robot.Robot):
 
 	new_game = PowerlaunchGame()
 
-    # drive_cozmo_straight(robot, 150, 50)
-    # new_game.identify_cubes_and_create_list(robot)
-    # new_game.stack_cubes(robot)
-    # new_game.make_cube_cycle_through_colors(robot, 10, new_game.list_of_identified_cubes[0])
+	drive_cozmo_straight(robot, 150, 50)
 
-    # new_game.move_into_launch_position(robot)
+	new_game.identify_cubes_and_create_list(robot)
+	
+	new_game.stack_cubes(robot)
+
+	robot.run_timed_behavior(cozmo.behavior.BehaviorTypes.KnockOverCubes, active_time=60)
+
+	# new_game.make_cube_cycle_through_colors(robot, 4, new_game.list_of_identified_cubes[0])
+
+	# new_game.move_into_launch_position(robot, distance_range_tuple, angle_range_tuple)
 
 	# new_game.user_defined_launch_power = int(input("\n\n\n-------->POWER! How many electroids will you give Cozmo (1-10)? "))
 	# print("-------->Charging Cozmo with", new_game.user_defined_launch_power, "electroids!\n\n\n")
 	
-	robot.move_lift(3)
-	time.sleep(3)
+	# robot.move_lift(3)
+	# time.sleep(3)
 
 	# new_game.launch_cozmo_towards_target(robot, distance_range_tuple, angle_range_tuple)
 
+	# if new_game.did_win:
+	#     print("You and Cozmo Win!")
+	# else:
+	# 	print("Try Again!")
+
 cozmo.run_program(cozmo_program)
-
-
-
-
-#BUGS
-#even when it doesn't stack cubes the light cycles
-#it likes to put the cube on and off indefinitely
 
 
